300dbab7e7ca6fb9409465fe5dad09642b90a3c5
ayoung0073 <69340410+ayoung0073@users.noreply.github.com>
2020-12-22 19:23:12 +0900

    프로젝트 생성

---
+++ src/main/java/com/doubleslash/sharedsurvey/SharedsurveyApplication.java
@@ -0,0 +1,13 @@
+package com.doubleslash.sharedsurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedsurveyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharedsurveyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doubleslash/sharedsurvey/SharedsurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doubleslash.sharedsurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedsurveyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
