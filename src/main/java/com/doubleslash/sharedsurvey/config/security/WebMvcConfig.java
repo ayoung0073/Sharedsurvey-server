48d159f7728cb233e2fdecff57b906e1263eaacb
ayoung0073 <ayong0310@naver.com>
2021-01-19 03:01:43 +0900

    Fix - cors 제한 해제

--- src/main/java/com/doubleslash/sharedsurvey/config/security/WebMvcConfig.java
+++ src/main/java/com/doubleslash/sharedsurvey/config/security/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.doubleslash.sharedsurvey.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,4 +16,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/file/**")
                 .addResourceLocations("file:///tmp/tomcat.8080.9056073029680764243/work/Tomcat/localhost/ROOT/"); //리눅스 root에서 시작하는 폴더 경로
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+
+    }
+
+
 }
