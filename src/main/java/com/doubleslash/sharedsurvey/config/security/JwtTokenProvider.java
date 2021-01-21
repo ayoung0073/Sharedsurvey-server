20fabb24619514703f2a1d49d795148439da4fb9
ayoung0073 <ayong0310@naver.com>
2021-01-21 13:33:37 +0900

    Fix - 토큰 유효시간 수정

--- src/main/java/com/doubleslash/sharedsurvey/config/security/JwtTokenProvider.java
+++ src/main/java/com/doubleslash/sharedsurvey/config/security/JwtTokenProvider.java
@@ -29,8 +29,8 @@ public class JwtTokenProvider {
 
     private String secretKey = "webfirewood";
 
-    // 토큰 유효시간 30분
-    private long tokenValidTime = 30 * 60 * 1000L;
+    // 토큰 유효시간 2시간으로 일단.. 120 -> 30
+    private long tokenValidTime = 120 * 60 * 1000L;
 
     private final MemberService userDetailsService;
 
